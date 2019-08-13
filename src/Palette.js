8ebb3e96772fdc88457fe2c36fa6074d033d2346
objectdotprototypedotjs <object.prototype.js@gmail.com>
2019-08-13 23:51:16 +0530

    ColorBox Added
    ColorBox added to the pallete with
    a width of 20% and a height of 25%.

--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-
+import ColorBox from './ColorBox'
+import './Palette.css';
 class Palette extends Component{
     render(){
+        const colorBoxes = this.props.colors.map(color => {
+            return <ColorBox background={color.color} name={color.name}/>
+        });
         return(
         <div className="Palette">
             {/* Navbar goes here */}
             <div className="Palette-colors">
                 {/* Bunch of color boxes */}
+                {colorBoxes}
+
             </div>
             {/* Footer goes here */}
         </div>
