d0c1c0e5d14de169dd42f7416cf9949a8e78c569
objectdotprototypedotjs <object.prototype.js@gmail.com>
2019-09-12 09:15:45 +0530

    Added SingleColorPalette Added SingleColorPalette and linked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import PaletteList from './PaletteList'
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
+import SingleColorPalette from './SingleColorPalette';
 
 class App extends Component{
   findPalette(id){
@@ -28,7 +29,7 @@ class App extends Component{
 <Route
 exact
 path="/palette/:paletteId/:colorId"
-render={() => <h1>Single Color Page</h1>} 
+render={() => <SingleColorPalette />} 
 />
 </Switch>
     );
