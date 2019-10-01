90442a4efc76595e3fd8c7fb51b934c85caad096
objectdotprototypedotjs <object.prototype.js@gmail.com>
2019-10-01 23:21:39 +0530

    NewPaletteForm added
    NewPaletteForm component added and
    linked

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 class App extends Component{
   findPalette(id){
@@ -15,6 +16,11 @@ class App extends Component{
   render(){
     return (
       <Switch>
+<Route 
+  exact
+  path="/palette/new"
+  render ={() => <NewPaletteForm />}
+/>
         
 <Route 
   exact 
